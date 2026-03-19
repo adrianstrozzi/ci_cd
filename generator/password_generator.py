546401e15797b34654a27915d54ca56d463af15a
Adrian Strozzi <adrianstrozzi@gmail.com>
2026-03-18 20:28:26 -0600

    Updated password generator

--- generator/password_generator.py
+++ generator/password_generator.py
@@ -24,3 +24,12 @@ def has_number(password):
 
 def has_symbol(password):
     return any(c in string.punctuation for c in password)
+
+def validate_password(password):
+    return has_uppercase(password) and has_number(password) and has_symbol(password)
+
+def generate_secure_password(length=12):
+    while True:
+        password = generate_password(length)
+        if validate_password(password):
+            return password
